6e6da541a8057c69fc1501a9c007c28bc0eceee4
sajaa45 <moussa.saja@gmail.com>
2025-06-26 13:30:24 +0100

    mbart comparision to marianmt added

--- translate/translate.py
+++ translate/translate.py
@@ -1,13 +1,31 @@
 from fastapi import FastAPI
 from transformers import MarianMTModel, MarianTokenizer
 import torch
+import time
+import tracemalloc
 
+# Start measuring time and memory
+start_time = time.time()
+tracemalloc.start()
 app = FastAPI()
 
 # Load model at startup
 model_name = "Helsinki-NLP/opus-mt-en-fr"
 tokenizer = MarianTokenizer.from_pretrained(model_name)
 model = MarianMTModel.from_pretrained(model_name)
+text = "This is a visa document."
+inputs = tokenizer(text, return_tensors="pt", padding=True)
+with torch.no_grad():
+    translated = model.generate(**inputs)
+output = tokenizer.decode(translated[0], skip_special_tokens=True)
+# Stop measuring memory and time
+current, peak = tracemalloc.get_traced_memory()
+end_time = time.time()
+tracemalloc.stop()
+
+print(f"Execution time: {end_time - start_time:.2f} seconds")
+print(f"Peak memory usage: {peak / 1024 / 1024:.2f} MB")
+print("translation:", output)
 
 @app.post("/translate")
 async def translate_text(text: str):
@@ -16,3 +34,4 @@ async def translate_text(text: str):
         translated = model.generate(**inputs)
     output = tokenizer.decode(translated[0], skip_special_tokens=True)
     return {"translation": output}
+
